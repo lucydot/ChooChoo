06157f93f27e81ccfa9ff39176012842e95b8647
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-10 20:00:12 +0000

    Create management.py

---
+++ choochoo/management.py
@@ -0,0 +1,14 @@
+"""
+A module for choochoo project management.
+
+A collection of admin functions.
+"""
+
+def check_instructor(handle):
+  pass
+
+def check_admin(handle):
+  pass
+
+def check_student(handle):
+  pass
