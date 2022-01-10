0ed604655ddcd0db7070f6bc321aa8491d5b07e7
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-10 09:11:29 +0000

    Create cli.py
    Outline for how this will parse the argument strings.

---
+++ choochoo/cli.py
@@ -0,0 +1,44 @@
+"""A simple command line interface for choochoo.
+
+Contains the single function `interface`.
+"""
+import re
+from sys import argv
+
+from choochoo import management, delivery
+
+def interface():
+  """ This function interfaces between the user and the choochoo package.
+  
+  It takes user sub-commands and parses them as a complete sentence. 
+  It matches this sentence to one within a pre-defined list, 
+  and then calls other choochoo functions as appropriate. 
+  If it does not match the sentence then a message is returned to the user.
+  """
+  
+  command = ' '.join(argv[1:])
+  
+  # returns true if the command string matches the argument string
+  command_matches = lambda string: bool(re.match(string, command))
+  
+  if command_matches("create (@\w+) as instructor"):
+    handle = argv[2]
+    if management.check_instructor(handle,silent=True) is False:
+      delivery.issue_reply("Choochoo is closing this issue as ",handle," \
+                           is not listed as an instructor in settings.yml.")
+      delivery.close_issue()
+    
+  if command_matches("check (@\w+) is instructor"):
+    handle = argv[2]
+    management.check_instructor(handle)
+    
+  elif command_matches("check (@\w+) is admin"):
+    handle = argv[2]
+    management.check_admin(handle)
+    
+  elif command_matches("check (@\w+) is student"):
+    handle = argv[2]
+    management.check_student(handle)
+    
+    
+  
