17c4283723e2a70aab438aae0e3c96ef84597889
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-11 19:34:08 +0000

    issue_reply -> make_comment

--- scripts/check_student_ticket.py
+++ scripts/check_student_ticket.py
@@ -10,6 +10,6 @@ from choochoo import management, issues
 
 handle = argv[1]
 if management.check_student(handle) is False:
-  issues.issue_reply("Choochoo is closing this issue as ",handle," \
+  issues.make_comment("Choochoo is closing this issue as ",handle," \
                        is not listed as an student in settings.yml.")
   issues.close_issue()
