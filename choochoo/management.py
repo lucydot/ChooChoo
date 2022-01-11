44a036351555a00117cbd63cb55cfeccbb8b1916
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-11 20:01:54 +0000

    Include get_token and get_repo functions

--- choochoo/management.py
+++ choochoo/management.py
@@ -3,7 +3,25 @@ A module for choochoo project management.
 
 A collection of admin functions.
 """
+import github
+import dotenv 
 
+from choochoo import settings
+
+
+def get_token():
+  "Get gh token, needed for the PyGithub API"
+  
+  dotenv.load_dotenv()
+  return os.getenv("GH_TOKEN")
+
+def get_repo(GH_TOKEN):
+  "Get a repo object using PyGithubAPI"
+  
+  project_repo = settings.get_project_repo()
+  g = github.Github(GH_TOKEN)
+  return g.get_repo(project_repo)
+  
 def check_instructor(handle):
   pass
 
