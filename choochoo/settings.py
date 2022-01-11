31a55220f421f21d167539dbb565f8bde1e7a4d0
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-11 20:24:46 +0000

    function for get_settings_path
    Using ROOT_DIR which is defined in __INIT__

--- choochoo/settings.py
+++ choochoo/settings.py
@@ -1,1 +1,12 @@
+"""
+A module for reading and writing to the settings yml file.
+"""
+import os
+from choochoo import definitions, ROOT_DIR
+
+def get_settings_path():
+  return os.path.join(ROOT_DIR, 'instructor', 'settings.yml')
+
+def get_project_repo():
+  pass
 
