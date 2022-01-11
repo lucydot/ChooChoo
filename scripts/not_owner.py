3e4474baea5f79c431ae20757d28b5f80eec6f68
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-11 09:08:11 +0000

    Add script string

--- scripts/not_owner.py
+++ scripts/not_owner.py
@@ -1,4 +1,9 @@
-from choochoo issues
+"""
+this script is called by .github/.workflows/fat_controller.yml when someone calls choochoo
+in an issue thread they did not author (create).
+"""
+
+from choochoo import issues
 
 issues.issue_reply("Choochoo is ignoring this command as you are not the \
                    issue author.")
