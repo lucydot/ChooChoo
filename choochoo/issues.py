de8ba603c485df191ac20253fc2230c005e7179e
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-19 15:09:34 +0000

    create issue class

--- choochoo/issues.py
+++ choochoo/issues.py
@@ -1,9 +1,30 @@
 """
 A module for reading from, and writing to, the issues tracker on a choochoo repo.
 """
+import github
+from choochoo import management
 
-def make_comment(message):
-  pass
+def issue_number():
+    dotenv.load_dotenv()
+    return os.getenv("ISSUE_NUMBER")
 
-def close_issue():
-  pass
+class Issue:
+    """Class for holding, reading from and writing to a
+    repository issue.
+    """
+
+    def __init__(self,
+                 repository,
+                 number):
+
+        self.repository = repository
+        self.number = number
+        self.pygh_issue = self.repository.pygh_repo.get_issue(self.number)
+
+
+    def make_comment(*message):
+        message = " ".join(message)
+        self.pygh_issue.create_comment(message)
+
+    def close_issue():
+        self.pygh_issue.edit(state=closed)
