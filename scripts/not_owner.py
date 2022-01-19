f82d7b12429a86bae191300c0fe387fc92270f79
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-01-19 15:11:35 +0000

    add a note to the scripts - I think these can be removed and incorporated into the github workflow

--- scripts/not_owner.py
+++ scripts/not_owner.py
@@ -3,6 +3,9 @@ this script is called by .github/.workflows/fat_controller.yml when someone call
 in an issue thread they did not author (create).
 """
 
+### NOTE: we don't need this script. Can make a comment from within the action: 
+# https://stackoverflow.com/questions/58066966/commenting-a-pull-request-in-a-github-action
+
 from choochoo import issues
 
 issues.make_comment("Choochoo is ignoring this command as you are not the \
