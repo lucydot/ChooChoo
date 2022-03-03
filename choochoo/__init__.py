f299962b42d370f0d8ee137b41c044ad0854262a
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-03 14:48:40 +0000

    Update __init__.py

--- choochoo/__init__.py
+++ choochoo/__init__.py
@@ -3,7 +3,7 @@ import os
 __version__ = "0.0.1"
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-settings_path = os.path.join(ROOT_DIR, "./instructor/settings.yml")
+settings_path = "./instructor/settings.yml"
 issue_template_path = os.path.join(ROOT_DIR, "./.github/ISSUE_TEMPLATE/choochoo-student-thread.md")
 objectives_header_path = os.path.join(ROOT_DIR, "./instructor/objectives_header.md")
 objectives_path = os.path.join(ROOT_DIR, "./instructor/objectives.yml")
