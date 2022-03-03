ff597ca860ca213be24ec4d25a0794c06977e0aa
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-03 15:21:46 +0000

    remove os.path for root_dir as is causing problems.
    This means choochoo always expects to be called from the root of the repo...

--- choochoo/__init__.py
+++ choochoo/__init__.py
@@ -1,9 +1,9 @@
-import os
+#import os
 
 __version__ = "0.0.1"
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+#ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 settings_path = "./instructor/settings.yml"
-issue_template_path = os.path.join(ROOT_DIR, "./.github/ISSUE_TEMPLATE/choochoo-student-thread.md")
-objectives_header_path = os.path.join(ROOT_DIR, "./instructor/objectives_header.md")
-objectives_path = os.path.join(ROOT_DIR, "./instructor/objectives.yml")
+issue_template_path = "./.github/ISSUE_TEMPLATE/choochoo-student-thread.md"
+objectives_header_path = "./instructor/objectives_header.md"
+objectives_path = "./instructor/objectives.yml"
