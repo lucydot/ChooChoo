43fc8657b99a1fb6244c6fc72bd753034c675b2f
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-03 11:32:02 +0000

    move all paths to init

--- choochoo/__init__.py
+++ choochoo/__init__.py
@@ -3,4 +3,7 @@ import os
 __version__ = "0.0.1"
 # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
+settings_path = "./instructor/settings.yml"
+issue_template_path = "./.github/ISSUE_TEMPLATE/choochoo-student-thread.md"
+objectives_header_path = "./instructor/objectives_header.md"
+objectives_path = "./instructor/objectives.yml"
