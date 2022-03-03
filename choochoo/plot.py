e0573f4a499a0bddd894b1fffca0c40feb513de4
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-03 10:53:22 +0000

    remove tick box parsing and put into management

--- choochoo/plot.py
+++ choochoo/plot.py
@@ -1,34 +1,10 @@
 """Module for parsing the student's checkboxes and
 generating a summary plot of class progress"""
 
-from choochoo import management
 import matplotlib.pyplot as plt
-import numpy as np
-import re
 
 issue_template_path = "./.github/ISSUE_TEMPLATE/choochoo-student-thread.md"
-output_file_path = "./plots/summary_plot.png"
-
-def parse_tickboxes():
-"""Parse issue data and sum up the number of ticked boxes for each objective.
-Return as a dictionary with each objective as the key and number of ticks as the value."""
-
-	issue_template_content = management.Repository.pygh_repo.get_contents(issue_template_path).decoded_content.decode()
-	objectives = re.findall(r'\[ ] (.*)\n', issue_template_content)
-	student_issues = repo.student_issues(labels=[issue_template_label])
-    tick_count = dict.fromkeys(objectives,0)
-
-	for issue in student_issues:
-	    body = issue.body
-	    for objective in tick_count.keys():
-	        if objective in body:
-	            splits = body.split(objective,maxsplit=2)
-	            if splits[0][-4:-1] == '[x]':
-	                tick_count[objective] += 1
-	        else:
-	            print("problem: '{}' not in string in issue # {}".format(task, issue.number))
-
-	return tick_count
+output_plot_path = "./plots/summary_plot.png"
 
 def create_plot(tick_count):
 	""" Create a bar chart to summarise class progress. Lists objectives and the number
@@ -37,9 +13,9 @@ def create_plot(tick_count):
 	num_objectives = len(tick_count)
 	fig, ax = plt.subplots(1,1,figsize=(20,num_objectives))
 
-	ax.barh(np.arange(num_objectives),tick_count.values(), align='center')
-	ax.set_yticks(np.arange(num_objectives))
-	ax.set_xticks(np.arange(issue_count+0.1))
+	ax.barh(range(num_objectives),tick_count.values(), align='center')
+	ax.set_yticks(range(num_objectives))
+	ax.set_xticks(range(issue_count+0.1))
 	ax.set_yticklabels(tick_count.keys())
 	ax.invert_yaxis()
 	ax.set_xlabel('# of checked boxes')
@@ -49,5 +25,5 @@ def create_plot(tick_count):
 	    item.set_fontsize(20)
 
 	plt.tight_layout()
-	plt.savefig(output_file_path,dpi=250)
+	plt.savefig(output_plot_path,dpi=250)
 
