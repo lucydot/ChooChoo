79408e84fcd0dded98855e82149ef176d90f09a3
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-05 13:01:18 +0000

    Create test_repository.py

---
+++ tests/test_repository.py
@@ -0,0 +1,12 @@
+import pytest
+from choochoo import repository
+
+# override the default filepaths that are applicable to the choochoo-template
+issue_template_path = './files/choochoo-student-thread.md'
+env_path = './files/.env'
+
+repo = repository.Repository()
+
+def test_parse_tickboxes():
+  pass
+  
