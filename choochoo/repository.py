5444f095818498e716eefbdf2bee23b96d7747a9
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-05 14:23:01 +0000

    Update repository.py

--- choochoo/repository.py
+++ choochoo/repository.py
@@ -44,7 +44,7 @@ class Repository:
         """Parse issue data and sum up the number of ticked boxes for each objective.
         Return as a dictionary with each objective as the key and number of ticks as the value."""
 
-        issue_template_content = file_content(issue_template_path)
+        issue_template_content = self.file_content(issue_template_path)
         objectives = re.findall(r'\[ ] (.*)\n', issue_template_content)
         student_issues = self.student_issues(labels=['student'])
         tick_count = dict.fromkeys(objectives,0)
