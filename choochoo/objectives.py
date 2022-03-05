90ad579a4ba850a4cc13644412d2bba9793edc33
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-03-05 14:10:22 +0000

    remove table formatting as breaks the checkboxes on gh flavoured md

--- choochoo/objectives.py
+++ choochoo/objectives.py
@@ -11,12 +11,12 @@ def string_generator(section,long_name, short_name, table=False):
     string_list = []
 
     if len(section[long_name]) == 1:
-        string_list.extend(("[",short_name,"](",section[long_name][0],") "))
+        string_list.extend(("[",long_name,"](",section[long_name][0],") "))
     else:
         for index,link in enumerate(section[long_name]):
             string_list.extend(("[",short_name,str(index+1),"](",link,") "))
 
-    string_list.append("|")
+    string_list.append(" | ")
 
     if len(section[long_name]) ==  0 and table is True:
         return "| "
@@ -38,15 +38,13 @@ def generate_student_thread():
 
     with open(issue_template_path,"a") as f:
         for section in objectives_dictionary['sections']:
-            print("\n\n### "+ section['name'],file=f)
-            print("|"+string_generator(section,'tutorials','Tutorial')
-                     +string_generator(section,'questions','Question')
-                     +string_generator(section,'links','Link'),file=f)
+            print("\n\n### "+ section['name']+" | "+string_generator(section,'tutorials','T')
+                     +string_generator(section,'questions','Q')
+                     +string_generator(section,'links','L'),file=f)
 
-            print("\n| | objective | tutorials | questions | links |\n|----|----|----|----|----|",file=f)
             for objective in section['objectives']:
-                print("|- [ ]| "+objective['name']+"|"
-                    +string_generator(objective,'tutorials','T',table=True)
-                    +string_generator(objective,'questions','Q',table=True)
-                    +string_generator(objective,'links','L',table=True),file=f)
+                print("- [ ] "+objective['name']+" | "
+                    +string_generator(objective,'tutorials','T',table=False)
+                    +string_generator(objective,'questions','Q',table=False)
+                    +string_generator(objective,'links','L',table=False),file=f)
 
