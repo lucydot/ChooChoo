6fefaf09ba16f09fc46b1ff0053301e93dbc615c
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-04-21 10:58:01 +0100

    include function for converting string to anchor link

--- choochoo/utilities.py
+++ choochoo/utilities.py
@@ -3,11 +3,17 @@
 
 def read_yaml(self,pathname):
 
-        with open(pathname, "r") as stream:
-            try:
+    with open(pathname, "r") as stream:
+        try:
                 dictionary = yaml.load(stream, Loader=yaml.FullLoader)
-            except yaml.YAMLError as exc:
+        except yaml.YAMLError as exc:
                 print(exc)
 
-        return dictionary
+    return dictionary
+
+def string_to_anchor_link(input_string):
+
+    s = input_string.translate(str.maketrans('', '', string.punctuation))
+    anchor_string = "-".join(s.lower().split())
+    return "(#"+anchor_string+")"
 
