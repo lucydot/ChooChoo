3227055bfcf3764e1e405f88be04e5254d1a58f5
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-05-03 15:20:58 +0100

    Update question.py

--- choochoo/question.py
+++ choochoo/question.py
@@ -82,6 +82,6 @@ class Question:
 
         dictionary_list = question_bank.QuestionBank(self.repository).get_dictionary_list()
         dictionary_list.append({"authors":self.authors,"title":self.title,"question":self.question,"answer":self.answer,"checklist_items":self.checklist_items})
-        with open(question_bank_path, 'w') as stream:
+        with open(question_bank_yml_path, 'w') as stream:
             yaml.dump(dictionary_list, stream)
       
