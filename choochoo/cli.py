de705b051db2e324d1030788305a71d0df0d7f35
Lucy Whalley <l.whalley@northumbria.ac.uk>
2022-05-08 16:09:07 +0100

    Update cli.py

--- choochoo/cli.py
+++ choochoo/cli.py
@@ -8,7 +8,7 @@ from sys import argv
 import random
 
 from choochoo import repository, issue, settings, environment, plot, objectives, question, question_bank
-from choochoo import question_folder_path, question_bank_path, question_bank_yml_path
+from paths import question_folder_path, question_bank_path, question_bank_yml_path
 
 def issue_interface():
     """ This function interfaces between the user and the choochoo package.
